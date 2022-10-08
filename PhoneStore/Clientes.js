704754a3e3436a2e2ff6fd65b1a0c4ef9f6df994
Eliub Rangel <eliubr47@gmail.com>
2022-10-07 20:09:22 -0500

    se hicieron cambion en clientes

--- PhoneStore/Clientes.js
+++ PhoneStore/Clientes.js
@@ -1,12 +1,37 @@
 console.log('iniciando script');
 var Clientes = [];
-var btnAgregar_click = function(){
-    alert('Soy el click del boton agregar')
-    var nombre = $('#txtNombre').val();
-    Clientes.push({
-        nombre: nombre
-    });
-    console.log(Clientes)
+let maxId = 0;
+const btnAgregar_click = function(){
+    const Nombre = $('#txtNombre').val();
+    let formValido = true;
+    if(Nombre.trim() === ''){
+        alert('Nombre requerido')
+        formValido = false;
+    }
+    
+    const Apellidos = $('#txtApellidos').val();
+    if(Apellidos.trim() === ''){
+        alert('Apellidos requeridos');
+        formValido = false;
+    }
+    var RequiereFactura = $('input[type="checkbox"]').is(":checked");
+    var Direccion = $('#txtPrecio').val();
+    var FechaNacimiento= $('#txtFecha').val();
+
+
+    if(formValido){
+        const nuevoCliente = {
+            Id : maxId++,
+            Nombre,
+            Apellidos,
+            RequiereFactura,    
+            Direccion,
+            FechaNacimiento
+        };
+        Clientes.push(nuevoCliente);
+        $('table').append('<tr class="row'+nuevoCliente.Id+'"><td>'+Nombre+'</td><td>'+Apellidos+'</td><td>'+Direccion+'</td><td>'+FechaNacimiento+'</td><td>'+RequiereFactura+'</td></tr>');
+    }
+
 };
 $(document).ready(function(){
     $('#btnAgregar').click(btnAgregar_click);
